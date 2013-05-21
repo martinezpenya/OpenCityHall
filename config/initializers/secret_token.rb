4e5897b83db14b5d069e59613f5c466f6a054760
David Martínez <david@martinezpenya.es>
2013-05-21 11:12:19 +0200

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCityHall::Application.config.secret_token = '91a8be32a0d3abc0c3804996fde2925266f58d9943d9e8e76c40d1a67f60f7347cc67b7583e0eab151c73089a5c44a433c6af49b90b5509cdff46c8aeeab4420'
