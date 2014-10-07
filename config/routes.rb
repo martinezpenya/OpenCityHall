0cbd77d71f54aa90cc7282d662b7f04d87973fc7
david <david@udavidmartinez.carlet.com>
2014-10-07 12:29:49 +0200

    Set default route to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ OpenCityHall::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'admin/dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
